06b2de6f7912cb71e547df7120263ac510f54cf0
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-01-10 13:45:33 -0800

    added product page animated bk img

--- assets/global.js
+++ assets/global.js
@@ -723,3 +723,149 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+// LUKE's DEV
+/*
+used in: main-product-settings.liquid
+Animate on scroll
+danny boy! Link: https://codepen.io/rachsmith/post/how-to-move-elements-on-scroll-in-a-way-that-doesn-t-suck-too-bad
+*/
+/* requestAnimationFrame poly */
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+/* END requestAnimationFrame poly */
+var _containerHeight = 4000;
+var _width, _height, _scrollHeight;
+var _movingElements = [];
+var _scrollPercent = 0;
+var pre = prefix();
+var _jsPrefix = pre.lowercase;
+if (_jsPrefix == "moz") _jsPrefix = "Moz";
+var _cssPrefix = pre.css;
+var _positions = [
+	{
+		name: "animate-bk-img_left",
+		start: {
+			percent: 0.01,
+			x: 0.05,
+			y: 0.05,
+		},
+		end: {
+			percent: 0.9,
+			x: 0.05,
+			y: 0.55,
+		},
+	},
+];
+
+resize();
+initMovingElements();
+
+function initMovingElements() {
+	for (var i = 0; i < _positions.length; i++) {
+		_positions[i].diff = {
+			percent: _positions[i].end.percent - _positions[i].start.percent,
+			x: _positions[i].end.x - _positions[i].start.x,
+			y: _positions[i].end.y - _positions[i].start.y,
+		};
+		_positions[i].target = {};
+		_positions[i].current = {};
+		var el = document.getElementsByClassName(
+			"animate-bk-img " + _positions[i].name
+		)[0];
+		_movingElements.push(el);
+	}
+}
+
+function resize() {
+	_width = window.innerWidth;
+	_height = window.innerHeight;
+	_scrollHeight = _containerHeight - _height;
+}
+
+function updateElements() {
+	for (var i = 0; i < _movingElements.length; i++) {
+		var p = _positions[i];
+		if (_scrollPercent <= p.start.percent) {
+			p.target.x = p.start.x * _width;
+			p.target.y = p.start.y * _containerHeight;
+		} else if (_scrollPercent >= p.end.percent) {
+			p.target.x = p.end.x * _width;
+			p.target.y = p.end.y * _containerHeight;
+		} else {
+			p.target.x =
+				p.start.x * _width +
+				((p.diff.x * (_scrollPercent - p.start.percent)) /
+					p.diff.percent) *
+					_width;
+			p.target.y =
+				p.start.y * _containerHeight +
+				((p.diff.y * (_scrollPercent - p.start.percent)) /
+					p.diff.percent) *
+					_containerHeight;
+		}
+
+		// lerp
+		if (!p.current.x) {
+			p.current.x = p.target.x;
+			p.current.y = p.target.y;
+		} else {
+			p.current.x = p.current.x + (p.target.x - p.current.x) * 0.1;
+			p.current.y = p.current.y + (p.target.y - p.current.y) * 0.1;
+		}
+		_movingElements[i].style[_jsPrefix + "Transform"] =
+			"translate3d(" + p.current.x + "px, " + p.current.y + "px, 0px)";
+	}
+}
+
+function loop() {
+	_scrollOffset = window.pageYOffset || window.scrollTop;
+	_scrollPercent = _scrollOffset / _scrollHeight || 0;
+	updateElements();
+
+	requestAnimationFrame(loop);
+}
+
+loop();
+
+window.addEventListener("resize", resize);
+
+/* prefix detection http://davidwalsh.name/vendor-prefix */
+
+function prefix() {
+	var styles = window.getComputedStyle(document.documentElement, ""),
+		pre = (Array.prototype.slice
+			.call(styles)
+			.join("")
+			.match(/-(moz|webkit|ms)-/) ||
+			(styles.OLink === "" && ["", "o"]))[1],
+		dom = "WebKit|Moz|MS|O".match(new RegExp("(" + pre + ")", "i"))[1];
+	return {
+		dom: dom,
+		lowercase: pre,
+		css: "-" + pre + "-",
+		js: pre[0].toUpperCase() + pre.substr(1),
+	};
+}
